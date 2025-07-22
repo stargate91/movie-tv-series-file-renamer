a44d2a38a55d3b927a750316cc483c50c1706178
stargate91 <leventegallx@gmail.com>
2025-07-22 15:12:17 +0200

    Implement logging of terminal inputs with timestamps into auto-created logs folder.

--- logger_setup.py
+++ logger_setup.py
@@ -3,23 +3,43 @@ import os
 from datetime import datetime
 
 def setup_logging(log_dir="logs"):
-    class Tee:
-        def __init__(self, *files):
-            self.files = files
+    class Logger:
+        def __init__(self, console, logfile):
+            self.console = console
+            self.logfile = logfile
+            self.buffer_console = ""
+            self.buffer_file = ""
 
-        def write(self, obj):
-            for f in self.files:
-                f.write(obj)
-                f.flush()
+        def write(self, text):
+            self.buffer_console += text
+            self.buffer_file += text
+            while "\n" in self.buffer_console:
+                line_console, self.buffer_console = self.buffer_console.split("\n", 1)
+                line_file, self.buffer_file = self.buffer_file.split("\n", 1)
+
+                self.console.write(line_console + "\n")
+                self.console.flush()
+
+                timestamp = datetime.now().strftime("[%Y-%m-%d %H:%M:%S] ")
+                self.logfile.write(timestamp + line_file + "\n")
+                self.logfile.flush()
 
         def flush(self):
-            for f in self.files:
-                f.flush()
+            if self.buffer_console:
+                self.console.write(self.buffer_console)
+                self.console.flush()
+                self.buffer_console = ""
+
+            if self.buffer_file:
+                timestamp = datetime.now().strftime("[%Y-%m-%d %H:%M:%S] ")
+                self.logfile.write(timestamp + self.buffer_file)
+                self.logfile.flush()
+                self.buffer_file = ""
 
     os.makedirs(log_dir, exist_ok=True)
     log_filename = datetime.now().strftime("log_%Y-%m-%d_%H-%M-%S.txt")
     log_path = os.path.join(log_dir, log_filename)
     log_file = open(log_path, "w", encoding="utf-8")
 
-    sys.stdout = Tee(sys.stdout, log_file)
-    sys.stderr = Tee(sys.stderr, log_file)
+    sys.stdout = Logger(sys.stdout, log_file)
+    sys.stderr = Logger(sys.stderr, log_file)
